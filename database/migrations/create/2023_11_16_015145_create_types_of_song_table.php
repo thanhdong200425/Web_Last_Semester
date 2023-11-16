5b71a2a7e3b0efb8b0eec76112215ddb7a6afe94
Thanh Dong <thanhdong200425@gmail.com>
2023-11-16 09:30:36 +0700

    Add migrations

--- database/migrations/create/2023_11_16_015145_create_types_of_song_table.php
+++ database/migrations/create/2023_11_16_015145_create_types_of_song_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('types_of_song', function (Blueprint $table) {
+            $table->id('type_id');
+            $table->string('type_name');
+            $table->engine = 'InnoDB';
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('types_of_song');
     }
 };
--- database/migrations/create/2023_11_16_015834_create_albumns_table.php
+++ database/migrations/create/2023_11_16_015834_create_albumns_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('albumns', function (Blueprint $table) {
+            $table->id('albumn_id');
+            $table->string('albumn_name');
+            $table->integer('number_songs');
+            $table->text('cover_photo');
+            $table->text('short_description')->nullable();
             $table->timestamps();
+            $table->engine = 'InnoDB';
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('albumns');
     }
 };
