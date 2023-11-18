723d4a0cb6680d96a852aa0dd546c5ad541c7b5f
Thanh Dong <thanhdong200425@gmail.com>
2023-11-18 08:18:45 +0700

    Added dashboard admin template

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Dashboard')
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function() {
+    return view('pages.login');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function() {
+    Route::get('/', function() {
+        return view('pages.dashboard');
+    });
 });
