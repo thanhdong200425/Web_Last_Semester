f4622ec4dc1ca793305c6fa986b843538f338eb7
Thanh Dong <thanhdong200425@gmail.com>
2023-12-04 14:38:24 +0700

    Fixed

--- database/migrations/update/2023_12_01_112133_update_users_table_part2.php
+++ database/migrations/update/2023_12_01_112133_update_users_table_part2.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        
-        Schema::table('users', function(Blueprint $table) {
-          //  $table->string('remember_token');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('remember_token');
             $table->string('origin_password');
         });
-        
+
 
     }
 
